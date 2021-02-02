ff98dc9308bcfc5bd41417c9fe252829f870e8bd
Shiny Grey Ltd <mac@shinygrey.com>
2021-02-02 20:55:24 +0000

    per page and concurrency defaults

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,10 @@ module.exports = {
 			resolve: "gatsby-source-wordpress-experimental",
 			options: {
 				url: "https://wp.shinygrey.com/graphql",
+				schema: {
+					perPage: 100,
+					requestConcurrency: 15
+				}
 			},
 		},
 		'gatsby-plugin-postcss',
